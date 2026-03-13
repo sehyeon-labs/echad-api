4d672a6d353608e6eaceef81c341a80ab30481c4
hyeon0114 <kshyeon114@gmail.com>
2026-03-13 16:40:53 +0900

    template test
    - template test

--- src/test/java/kr/co/onmediagroup/template/service/PostServiceTest.java
+++ src/test/java/kr/co/onmediagroup/template/service/PostServiceTest.java
@@ -82,4 +82,6 @@ class PostServiceTest {
         // When & Then
         assertThrows(PostException.UnauthorizedPostAccess.class, () -> postService.changeActive(postId, userId, activeYn));
     }
+
+    // template test
 }
